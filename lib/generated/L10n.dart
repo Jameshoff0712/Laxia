9243a3853efb50b98713b744a331cb9c3bbd8b78
AsaiTadao <blackhole.rsb@gmail.com>
2022-03-17 01:23:10 +0900

    l10n1

--- lib/generated/L10n.dart
+++ lib/generated/L10n.dart
@@ -49,57 +49,6 @@ class Trans {
   static Trans? maybeOf(BuildContext context) {
     return Localizations.of<Trans>(context, Trans);
   }
-// GENERATED CODE - DO NOT MODIFY BY HAND
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'intl/messages_all.dart';
-
-// **************************************************************************
-// Generator: Flutter Intl IDE plugin
-// Made by Localizely
-// **************************************************************************
-
-// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
-// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
-// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
-
-class Trans {
-  Trans();
-
-  static Trans? _current;
-
-  static Trans get current {
-    assert(_current != null,
-        'No instance of Trans was loaded. Try to initialize the Trans delegate before accessing Trans.current.');
-    return _current!;
-  }
-
-  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
-
-  static Future<Trans> load(Locale locale) {
-    final name = (locale.countryCode?.isEmpty ?? false)
-        ? locale.languageCode
-        : locale.toString();
-    final localeName = Intl.canonicalizedLocale(name);
-    return initializeMessages(localeName).then((_) {
-      Intl.defaultLocale = localeName;
-      final instance = Trans();
-      Trans._current = instance;
-
-      return instance;
-    });
-  }
-
-  static Trans of(BuildContext context) {
-    final instance = Trans.maybeOf(context);
-    assert(instance != null,
-        'No instance of Trans present in the widget tree. Did you add Trans.delegate in localizationsDelegates?');
-    return instance!;
-  }
-
-  static Trans? maybeOf(BuildContext context) {
-    return Localizations.of<Trans>(context, Trans);
-  }
   /// `OFF`
   String get offer_off {
     return Intl.message(
@@ -181,33 +130,6 @@ class Trans {
       args: [],
     );
   }
-}
-
-class AppLocalizationDelegate extends LocalizationsDelegate<Trans> {
-  const AppLocalizationDelegate();
-
-  List<Locale> get supportedLocales {
-    return const <Locale>[
-      Locale.fromSubtags(languageCode: 'en'),
-    ];
-  }
-
-  @override
-  bool isSupported(Locale locale) => _isSupported(locale);
-  @override
-  Future<Trans> load(Locale locale) => Trans.load(locale);
-  @override
-  bool shouldReload(AppLocalizationDelegate old) => false;
-
-  bool _isSupported(Locale locale) {
-    for (var supportedLocale in supportedLocales) {
-      if (supportedLocale.languageCode == locale.languageCode) {
-        return true;
-      }
-    }
-    return false;
-  }
-}
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<Trans> {
