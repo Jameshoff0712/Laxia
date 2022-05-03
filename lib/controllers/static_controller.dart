f77e422aa1f70fe32b596150ee33b5f7bd3298e7
AsaiTadao <blackhole.rsb@gmail.com>
2022-05-03 17:52:17 +0900

    add_static_data_api

--- lib/controllers/static_controller.dart
+++ lib/controllers/static_controller.dart
@@ -1,4 +1,5 @@
 import 'package:laxia/controllers/base_controller.dart';
+import 'package:laxia/models/static/areas_model.dart';
 import 'package:laxia/models/static/master_model.dart';
 import 'package:laxia/services/http/static_api.dart';
 
@@ -8,5 +9,8 @@ class StaticController extends BaseController {
   StaticController();
   Future<List<Master_Model>> getTreatCategories() async {
     return await api.getTreatCategories();
+  }
+  Future<List<Area_Model>> getAreas() async {
+    return await api.getAreas();
   }
 }
--- lib/services/http/static_api.dart
+++ lib/services/http/static_api.dart
@@ -1,3 +1,4 @@
+import 'package:laxia/models/static/areas_model.dart';
 import 'package:laxia/models/static/master_model.dart';
 import 'package:laxia/services/http/api.dart';
 
@@ -8,5 +9,10 @@ class HomeApi extends Api {
     String? token = await preferenceUtil.getToken();
     final res = await Api.get("$apiUrl/load/master", token);
     return List<Master_Model>.from(res.data["data"]["treatCategories"].map((x) => Master_Model.fromJson(x as Map<String, dynamic>)) as Iterable<dynamic>);
+  }
+  Future<List<Area_Model>> getAreas() async {
+    String? token = await preferenceUtil.getToken();
+    final res = await Api.get("$apiUrl/load/areas", token);
+    return List<Area_Model>.from(res.data["data"].map((x) => Area_Model.fromJson(x as Map<String, dynamic>)) as Iterable<dynamic>);
   }
 }
