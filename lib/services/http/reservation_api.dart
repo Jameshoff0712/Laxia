2315b8e5e9610172df98bb4ac1435861d7405435
AsaiTadao <blackhole.rsb@gmail.com>
2022-07-17 10:39:51 -0700

    add chat

--- lib/services/http/reservation_api.dart
+++ lib/services/http/reservation_api.dart
@@ -3,15 +3,15 @@ import 'package:laxia/models/status_model.dart';
 import 'package:laxia/models/token_model.dart';
 import 'package:laxia/services/http/api.dart';
 
+
 class ReservationApi extends Api {
   ReservationApi();
 
-  Future<StatusInfo> allStatus() async {
+  Future<List<StatusInfo>> allStatus() async {
     String? token = await preferenceUtil.getToken();
-    print(token);
+    // print(token);
     final res = await Api.get("$apiUrl/reservations?status=", token);
-    print(StatusInfo.fromJson(res.data));
-    return StatusInfo.fromJson(res.data);
+    return List<StatusInfo>.from(res.data["data"].map((x) => StatusInfo.fromJson(x as Map<String, dynamic>)) as Iterable<dynamic>);
   }
 
   Future<void> reserve(ReservePost rsv) async {
